1bee1ccb6565093aa0b09ff2cef8dbfabad80d9a
Omar Jaseff Bautista Bazán <40043595+jaseff2805@users.noreply.github.com>
2025-04-15 03:22:11 +0000

    Ejercicio de Generador de Dominiod

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
-import "bootstrap";
-import "./style.css";
+let pronoun = ['the', 'our']
+let adj = ['great', 'big']
+let noun = ['jogger', 'racoon']
+let extenxion = ['.he','.ur','.at','.ig','.er','.on']
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let p of pronoun) {
+    for (let a of adj) {
+        for (let n of noun) {
+            for (let e of extenxion) {
+              if (n.endsWith(e.slice(1))) { // .endsWith junto con .slice sirve para identificar si el noun "n" termina con alguna de las extensiones
+                let hackNoun = n.slice(0, -e.length + 1)
+                console.log(p + a + hackNoun + e + ' ---Dominio Hackeado---')
+              } else {
+                console.log(p + a + n + e);
+              }  
+            }
+        }
+    }
+}
